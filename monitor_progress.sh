0f28fca1d3dde038c2706995241c167897b8a0c9
Vitaly Litvak <vitavaque@gmail.com>
2012-11-26 00:20:09 +0400

    Added simple progress monitor shell script
    Added firs shell script that monitors progress with du command.
    See also
    http://vitalidze.github.com/2012/11/25/Simple-progress-bar-in-bash/

---
+++ monitor_progress.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+to_monitor=$1
+target_gb=$2
+read_mb=0
+while [ $read_mb -le $(( $target_gb*1024 )) ]
+do
+        read_mb=`du -sm $to_monitor | cut -f 1`
+        read_gb=$(( $read_mb/1024 ))
+        echo -ne '[ '
+        for (( gb=0; gb<$target_gb; gb++ ))
+                do
+                        if [ $read_gb -ge $gb ]
+                        then
+                                echo -ne '#';
+                        else
+                                echo -ne ' '
+                        fi
+                done
+        perc=$(( $read_mb*100/($target_gb*1024) ))
+        echo -ne " $perc%] $read_mb of $(($target_gb*1024))\r"
+        sleep 1
+done
